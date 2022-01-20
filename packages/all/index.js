8a281d66e2be7eb521e25e0fe2135df60d8f46f1
Daniel Galindo <hello@danielgalindo.dev>
2022-01-20 14:04:00 -0600

    general eslint config for ts and nuxt

---
+++ packages/all/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@growrk/eslint-config-nuxt',
+  ],
+}
---
+++ packages/nuxt/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    '@growrk/eslint-config-ts',
+  ],
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+  },
+}
