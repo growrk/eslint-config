b03f29c79b41b9176aa76c67410177aa2f0d72dd
Daniel Galindo <hello@danielgalindo.dev>
2022-01-20 17:20:12 -0600

    fix no use before define

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -226,7 +226,7 @@ module.exports = {
     // Use new when throwing error
     'unicorn/throw-new-error': 'error',
 
-    'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
+    'no-use-before-define': ['error', { functions: true }],
     'eslint-comments/disable-enable-pair': 'off',
     'import/no-named-as-default-member': 'off',
 
