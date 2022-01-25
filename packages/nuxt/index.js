ca15f4980bcf61378274356b8a6e46b5eecbb7dd
Daniel Galindo <hello@danielgalindo.dev>
2022-01-24 18:50:44 -0600

    fix lint

--- packages/nuxt/index.js
+++ packages/nuxt/index.js
@@ -1,16 +1,16 @@
 module.exports = {
   plugins: ['prettier'],
   extends: [
-    '@nuxtjs/eslint-config-typescript'
+    '@nuxtjs/eslint-config-typescript',
   ],
   rules: {
-    semi: ['error', 'always'],
+    'semi': ['error', 'always'],
     'no-console': ['error', { allow: ['log', 'error'] }],
     '@typescript-eslint/no-unused-vars': [
       'error',
-      { vars: 'all', args: 'after-used', argsIgnorePattern: 'reject|context' }
+      { vars: 'all', args: 'after-used', argsIgnorePattern: 'reject|context' },
     ],
     // TypeScript compilation already ensures that named imports exist in the referenced module
-    'import/named': 'off'
-  }
-}
+    'import/named': 'off',
+  },
+};
